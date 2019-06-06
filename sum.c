b30c5116f25ddc710a70e4b33dd7a694c425967a
divyavs33 <51438539+divyavs33@users.noreply.github.com>
2019-06-06 14:35:04 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int a,b,sum;
+void getinput()
+ {
+   printf("Enter the two elements:");
+    scanf("%d %d",&a,&b);
+  }
+ int add()
+ {
+   int sum=a+b;
+   return sum;
+ }
+
+ void putinput()
+ {
+  printf("The sum of num is:%d",sum);
+  void getinput();
+   int add();
+ }
+
+void main()
+ {
+ void putinput();
+ } 
+   
